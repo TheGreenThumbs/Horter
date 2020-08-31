25fb022511e968cdb79e86214b492238b70505b5
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-31 16:43:25 -0500

    Dummy Data Function up and running

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ const { app } = require("./app");
 const PORT = process.env.PORT || 8080;
 
 // If mode is development drop database when the app starts
-const eraseDatabaseOnSync = process.env.NODE_ENV === "development";
+const eraseDatabaseOnSync = process.env.RESET_DB === "true";
 
+// Sync the Database and drop all the tables if RESET_DB is true
 sequelize.sync({ force: eraseDatabaseOnSync }).then(() => {
+  // If we reset the db fill it with dummy data
   if (eraseDatabaseOnSync) {
     fillDatabaseWithDummyData();
   }
