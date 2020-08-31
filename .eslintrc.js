e5a523b933b6f393d95886f1f4b673163d63f1e9
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-08-31 11:23:18 -0500

    Scaffolding complete

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['airbnb-base', 'prettier'],
+  env: { node: true },
+}
---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import App from './components/App.vue'
+
+new Vue ({
+  render: create => create(App),
+}).$mount('#app');
