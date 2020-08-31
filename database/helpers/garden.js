5bd9f86981dac42aa0577a97f14c673becdfdc04
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-31 15:34:32 -0500

    Update Garden info and add plant to garden added

--- database/helpers/garden.js
+++ database/helpers/garden.js
@@ -10,8 +10,8 @@ const { Garden } = models;
 const createGarden = (info) =>
   new Promise((resolve, reject) => {
     Garden.create({ ...info })
-      .then((data) => {
-        resolve(data);
+      .then((garden) => {
+        resolve(garden);
       })
       .catch((err) => {
         reject(err);
@@ -26,8 +26,47 @@ const createGarden = (info) =>
 const findGardenById = (id) =>
   new Promise((resolve, reject) => {
     Garden.findOne({ where: { id }, rejectOnEmpty: true })
-      .then((data) => {
-        resolve(data);
+      .then((garden) => {
+        resolve(garden);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+
+/**
+ * Add a plant to a garden, doesn't work until plants table exists
+ *  will throw an error if the garden isn't found
+ * @param {Integer} id garden id to add the plant to
+ * @param {Object} info information about the new plant
+ * @returns {Plant} Plant Info or an Error if garden isn't found
+ */
+const addPlantToGarden = (id, info) =>
+  new Promise((resolve, reject) => {
+    findGardenById(id)
+      .then((garden) => {
+        return garden.createPlant({ ...info });
+      })
+      .then((plant) => {
+        resolve(plant);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+
+/**
+ * Updates a garden with the given information based on garden id
+ *  throws an error if no garden is found
+ * @param {Integer} id id of garden to update
+ * @param {*} info the information about the garden to update
+ */
+const updateGardenInfo = (id, info) =>
+  new Promise((resolve, reject) => {
+    Garden.update({ ...info }, { where: { id }, returning: true })
+      .then(({ rows, garden }) => {
+        if (rows <= 0) throw new Error("Garden doesn't exist");
+        resolve(garden);
       })
       .catch((err) => {
         reject(err);
@@ -37,4 +76,6 @@ const findGardenById = (id) =>
 module.exports = {
   createGarden,
   findGardenById,
+  addPlantToGarden,
+  updateGardenInfo,
 };
