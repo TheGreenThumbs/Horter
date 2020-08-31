5cefeaf91dae131825f8a2395e867841c489e347
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-08-31 14:36:21 -0500

    express server serving build file on PORT

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', express.static(path.join(__dirname, '../client/build')))
+
+app.get('/', (req, res) => {
+  //  res.send('Hello World');
+   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+module.exports = {
+  app
+}
