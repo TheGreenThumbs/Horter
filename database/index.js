9b4ad09b389938c7f2a881613f19e72d8fbd607e
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-08-31 14:56:55 -0500

    Garden Model Added

--- database/index.js
+++ database/index.js
@@ -1,5 +1,7 @@
-const sequelize = require("sequelize");
-const { Sequelize } = require("sequelize");
+const Sequelize = require("sequelize");
+const factories = require("./models");
+
+const { gardenFactory } = factories;
 
 const dbHost = process.env.DB_HOST || "localhost";
 const dbName = process.env.DB_NAME || "horter";
@@ -13,7 +15,10 @@ const sequelize = new Sequelize(dbName, dbUser, dbPass, {
   dialect: "postgres",
 });
 
-const models = {};
+// Setup all the models with their factory functions
+const models = {
+  Garden: gardenFactory(sequelize),
+};
 
 // A function that sets up all the model associations, so we can drop the database
 //  while testing and then just call this function to reset all the associations
