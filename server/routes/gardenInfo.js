652c5788ec5c15d3ae44db147c30a414f41cdf1c
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-08-31 19:34:17 -0500

    Added route to send back single garden info from DB

--- server/routes/gardenInfo.js
+++ server/routes/gardenInfo.js
@@ -1,8 +1,25 @@
-const { Router } = require('express');
+const { Router } = require("express");
+
+const { findGardenById } = require("../../database/helpers/garden");
 
 const gardenInfo = Router();
-//GET/gardId grabs info for one user garden
+/**
+ * This route accepts a garden id from the client and sends the info for that garden from the DB
+ * @param {object} req.query the garden id value is stored at the 'id' key in req.query
+ * @returns {object} this is the garden obj from the DB
+ */
+gardenInfo.get("/one", (req, res) => {
+  const { id } = req.query;
+  findGardenById(id).then((garden) => {
+    res.send(garden);
+  });
+});
+
+//  POST/gardId/plantId add a plant to garden
+// gardenInfo.post("/add", (req, res) => {
+
+// });
 
 module.exports = {
   gardenInfo,
-}
+};
