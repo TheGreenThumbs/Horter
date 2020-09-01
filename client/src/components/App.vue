69fc5b0ab8f4c5497fa90a58edc851d6a3325b7c
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-09-01 17:10:35 -0500

    Garden Layout initial setup

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -1,8 +1,25 @@
 <template>
-  <div>Hello</div>
+  <div>
+    Hello
+    <garden-layout :gardenSize="gardenSize" :plants="plantList"></garden-layout>
+  </div>
 </template>
 <script>
+import GardenLayout from "./GardenLayout.vue";
 export default {
-  
-}
+  components: {
+    "garden-layout": GardenLayout,
+  },
+  data() {
+    return {
+      gardenSize: { width: 10, height: 10 },
+      plantList: [
+        { position_x: 1, position_y: 2, radius: 1 },
+        { position_x: 4, position_y: 4, radius: 1 },
+        { position_x: 1, position_y: 5, radius: 2 },
+      ],
+      selected: -1,
+    };
+  },
+};
 </script>
---
+++ client/src/components/GardenLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <canvas id="garden-layout" :height="gardenHeight" :width="gardenWidth">
+  </canvas>
+</template>
+
+<script>
+export default {
+  name: "gardenLayout",
+  props: {
+    gardenSize: {
+      validator: (info) => {
+        return info.width && info.height;
+      },
+    },
+    plants: {
+      validator: (list) =>
+        list.every(
+          (plant) => !!plant.position_x && !!plant.position_y && !!plant.radius
+        ),
+    },
+  },
+  computed: {
+    gardenHeight: function () {
+      const height = this.gardenSize.height * 10;
+      return `${height}px`;
+    },
+    gardenWidth: function () {
+      const width = this.gardenSize.width * 10;
+      return `${width}px`;
+    },
+  },
+};
+</script>
+
+<style lang="sass">
+#garden-layout
+  position: relative
+  background-color: $brown
+</style>
