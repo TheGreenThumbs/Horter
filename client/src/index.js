fb0f2623055eccd02a7bd919e010597e668ebe7c
Lorie Zepeda <lzepeda.it@gmail.com>
2020-09-02 15:07:48 -0500

    Added buefy and icons to app

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
-import Vue from 'vue'
-import App from './components/App.vue'
+import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import App from "./components/App.vue";
 
-new Vue ({
-  render: create => create(App),
-}).$mount('#app');
+// Make Buefy and Material Design Icons available to app
+Vue.use(Buefy, {
+  defaultIconPack: "mdi",
+});
+
+new Vue({
+  render: (create) => create(App),
+}).$mount("#app");
