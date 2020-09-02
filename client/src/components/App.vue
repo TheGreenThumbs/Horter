bdb85ebceb39db59c57d361e87b85c2c0268cac1
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-09-02 13:42:26 -0500

    added router to app component

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>Hello</div>
+  <div id="appvue">
+    <nav>
+      <router-link to="/garden">Garden Main</router-link>
+    </nav>
+    <h1>{{ msg }}</h1>
+    <router-view></router-view>
+  </div>
 </template>
+
 <script>
+import router from "../router";
 export default {
-  
-}
+  name: "app",
+  router,
+  data() {
+    return {
+      msg: "Welcome to Horter",
+    };
+  },
+};
 </script>
