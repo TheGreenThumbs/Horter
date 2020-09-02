ea1d2f3d33f295e8e3351b4c5e409a626110965c
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-09-02 17:03:06 -0500

    Basic Layout

--- client/src/components/GardenMain.vue
+++ client/src/components/GardenMain.vue
@@ -1,24 +1,41 @@
 <template>
-  <div>
-    <h1>GARDEN NAME</h1>
-    <p>
-      Florem ipsum sneezeweed peruvian lily dune helleborine plumed thistle.
-      Scabious st john’s wort holy grass false beard. Bloom glory lily drumstick
-      gentian buttercup cosmos foxtail lily
-    </p>
-    <garden-layout
-      :selected.sync="selected"
-      :gardenSize="gardenSize"
-      :plants="plantList"
-      v-on:plant-moved="plantMoved"
-    ></garden-layout>
-    <h2>PLANT NAME</h2>
-    <p>
-      Florem ipsum sneezeweed peruvian lily dune helleborine plumed thistle.
-      Scabious st john’s wort holy grass false beard.
-    </p>
-    <button>Add Plant</button>
-    <button>Edit Garden</button>
+  <div class="card">
+    <div class="card-header">
+      <p class="card-header-title">GARDEN NAME</p>
+    </div>
+    <div class="card-content">
+      <p>
+        Florem ipsum sneezeweed peruvian lily dune helleborine plumed thistle.
+        Scabious st john’s wort holy grass false beard. Bloom glory lily
+        drumstick gentian buttercup cosmos foxtail lily
+      </p>
+      <garden-layout
+        :selected.sync="selected"
+        :gardenSize="gardenSize"
+        :plants="plantList"
+        v-on:plant-moved="plantMoved"
+      ></garden-layout>
+      <article class="media">
+        <figure class="media-left">
+          <p class="image is-64x64">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong>PLANT NAME</strong><br />
+              Florem ipsum sneezeweed peruvian lily dune helleborine plumed
+              thistle. Scabious st john’s wort holy grass false beard.
+            </p>
+          </div>
+        </div>
+      </article>
+    </div>
+    <div class="card-footer">
+      <button class="card-footer-item">Add Plant</button>
+      <button class="card-footer-item">Edit Garden</button>
+    </div>
   </div>
 </template>
 
