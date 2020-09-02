4c7b0ad11ebc44c6f3df66f71be704e548ffc8d1
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-09-02 17:17:49 -0500

    Chatbot component rendering messages. for now attached to App.vue

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <navigation></navigation>
+    <chatbot></chatbot>
   </div>
 </template>
 
 <script>
 import Navigation from "./Navigation.vue";
+import Chatbot from "./Chatbot.vue";
 import router from "../router";
 
 export default {
   name: "App",
   router,
   components: {
-     navigation: Navigation
+    navigation: Navigation,
+    chatbot: Chatbot,
   },
 };
 </script>
---
+++ client/src/components/Chatbot.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="divclass">
+    <ul v-for="(m, index) in messages" v-bind:key="index">
+      <div>
+        <li>{{ m[0] }}: {{ m[1] }}</li>
+      </div>
+    </ul>
+    <message v-on:submit:message="submitMessage" />
+  </div>
+</template>
+
+<script>
+import Message from "./Message.vue";
+
+export default {
+  name: "chatbot",
+  components: {
+    Message,
+  },
+  data: function () {
+    return {
+      messages: [["ChatBot", "Hey there! How can I help?"]],
+    };
+  },
+  methods: {
+    submitMessage(message) {
+      // this should ideally contact the chatbot on submit with the message, then print the response
+      this.messages.push(["User", message], ["ChatBot", "no prob!"]);
+    },
+  },
+};
+</script>
