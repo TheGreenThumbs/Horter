83ca90f6aa7af83c4254b925c383c5bdd909d557
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-09-02 15:42:32 -0500

    Components moved to garden main page

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -5,44 +5,18 @@
     </nav>
     <h1>{{ msg }}</h1>
     <router-view></router-view>
-    <garden-layout
-      :selected.sync="selected"
-      :gardenSize="gardenSize"
-      :plants="plantList"
-      v-on:plant-moved="plantMoved"
-    ></garden-layout>
   </div>
 </template>
 
 <script>
 import router from "../router";
-import GardenLayout from "./GardenLayout.vue";
 export default {
   name: "App",
-  components: {
-    "garden-layout": GardenLayout,
-  },
+  router,
   data() {
     return {
-      gardenSize: { width: 10, height: 10 },
-      plantList: [
-        { position_x: 1, position_y: 2, radius: 2, id: 1 },
-        { position_x: 5, position_y: 4, radius: 2, id: 2 },
-        { position_x: 1, position_y: 5, radius: 4, id: 3 },
-      ],
-      selected: -1,
       msg: "Welcome to Horter",
     };
-  },
-  methods: {
-    plantMoved: function (info, collide) {
-      this.plantList.forEach((plant, i) => {
-        if (plant.id === this.selected) {
-          const newPlant = { ...plant, ...info };
-          this.$set(this.plantList, i, { ...plant, ...info });
-        }
-      });
-    },
   },
 };
 </script>
--- client/src/components/GardenMain.vue
+++ client/src/components/GardenMain.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>{{ msg }}</div>
+  <div>
+    <garden-layout
+      :selected.sync="selected"
+      :gardenSize="gardenSize"
+      :plants="plantList"
+      v-on:plant-moved="plantMoved"
+    ></garden-layout>
+  </div>
 </template>
 
 <script>
+import GardenLayout from "./GardenLayout.vue";
+
 export default {
   name: "GardenMain",
+  components: {
+    "garden-layout": GardenLayout,
+  },
   data() {
     return {
+      gardenSize: { width: 10, height: 10 },
+      plantList: [
+        { position_x: 1, position_y: 2, radius: 2, id: 1 },
+        { position_x: 5, position_y: 4, radius: 2, id: 2 },
+        { position_x: 1, position_y: 5, radius: 4, id: 3 },
+      ],
+      selected: -1,
       msg: "Garden Main Page",
     };
+  },
+  methods: {
+    plantMoved: function (info, collide) {
+      this.plantList.forEach((plant, i) => {
+        if (plant.id === this.selected) {
+          const newPlant = { ...plant, ...info };
+          this.$set(this.plantList, i, { ...plant, ...info });
+        }
+      });
+    },
   },
 };
 </script>
