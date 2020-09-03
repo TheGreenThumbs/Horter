63955aeb87b3928a7096e2fb50972e4a4c7d9ce1
Lorie Zepeda <lzepeda.it@gmail.com>
2020-09-02 20:27:15 -0500

    Added Wish List page

---
+++ client/src/components/WishList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <p class="card-header-title">WISH LIST</p>
+    </div>
+    <div class="card-content"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Wish",
+  methods: {
+    searchIconClick() {
+      alert("You wanna make a search?");
+    },
+    clearIconClick() {
+      this.search = "";
+      alert("Email cleared!");
+    },
+  },
+};
+</script>
