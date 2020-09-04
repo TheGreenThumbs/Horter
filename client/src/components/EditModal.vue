54928301d3b65e02ec4dc27cb90d1ddb6544d7ec
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-09-04 17:00:26 -0500

    (feat) created EditModal file which renders the editor modal

---
+++ client/src/components/EditModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="buttons">
+    <button class="button" @click="cardModal()">Edit Garden</button>
+  </div>
+</template>
+
+<script>
+import Editor from "./Editor.vue";
+export default {
+  name: "EditModal",
+  methods: {
+    cardModal() {
+      this.$buefy.modal.open({
+        parent: this,
+        component: Editor,
+        hasModalCard: false,
+        customClass: "custom-class custom-class-2",
+        trapFocus: false,
+        "full-screen": false,
+        width: 400,
+      });
+    },
+  },
+};
+</script>
