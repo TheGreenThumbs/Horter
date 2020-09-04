a94b91ba579e6ff7a840ee52bf3d5f605e4975a2
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-09-04 12:22:33 -0500

    (test) Db integration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-// const { sequelize } = require("../database");
-// const fillDatabaseWithDummyData = require("../testData");
+const { sequelize } = require("../database");
+const fillDatabaseWithDummyData = require("../testData");
 const logger = require("../winston");
 
 const { app } = require("./app");
@@ -9,21 +9,25 @@ const PORT = process.env.PORT || 8080;
 
 // If mode is development drop database when the app starts
 // This is for testing
-// const eraseDatabaseOnSync = process.env.NODE_ENV === "development";
+const eraseDatabaseOnSync = process.env.NODE_ENV === "development";
 
-app.listen(PORT, () => logger.info("server RUninning"));
 // Sync the Database and drop all the tables if RESET_DB is true
-// sequelize.sync({ force: eraseDatabaseOnSync }).then(() => {
-//   // If we reset the db fill it with dummy data
-//   if (eraseDatabaseOnSync) {
-//     fillDatabaseWithDummyData();
-//   }
+sequelize
+  .sync({ force: eraseDatabaseOnSync })
+  .then(() => {
+    // If we reset the db fill it with dummy data
+    if (eraseDatabaseOnSync) {
+      fillDatabaseWithDummyData();
+    }
 
-//   app
-//     .listen(PORT, () => {
-//       logger.info("Server running at: http://localhost:%s", PORT);
-//     })
-//     .catch((err) => {
-//       logger.error(err);
-//     });
-// });
+    app
+      .listen(PORT, () => {
+        logger.info("Server running at: http://localhost:%s", PORT);
+      })
+      .catch((err) => {
+        logger.error(err);
+      });
+  })
+  .catch((err) => {
+    logger.error(err);
+  });
