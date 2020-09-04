a26897dfa8a761b00089db953f7ec04b4933c368
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-09-04 16:59:30 -0500

    (feat) added EditGarden component which is the edit form with input boxes

---
+++ client/src/components/EditGarden.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="edit">
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label>Garden Name</label>
+        <input v-model="gardenName" />
+      </div>
+      <div>
+        <label>Latitude</label>
+        <input v-model="latitude" />
+      </div>
+      <div>
+        <label>Longitude</label>
+        <input v-model="longitude" />
+      </div>
+      <div>
+        <label>Width</label>
+        <input v-model="width" />
+      </div>
+      <div>
+        <label>Height</label>
+        <input v-model="height" />
+        <button>Update</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditGarden",
+  data() {
+    return {
+      gardenName: "",
+      latitude: "",
+      longitude: "",
+      width: "",
+      height: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit("edit:garden", {
+        gardenName: this.gardenName,
+        latitude: this.latitude,
+        longitude: this.longitude,
+        width: this.width,
+        height: this.height,
+      });
+    },
+  },
+};
+</script>
