35be1877db42f48221764c9ab9c8442bd7a6434e
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-09-05 12:42:05 -0500

    (refactor) passing full props to EditGarden. changed name of props keys

--- client/src/components/Editor.vue
+++ client/src/components/Editor.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <EditGarden v-on:edit:garden="submitEdit" />
+    <EditGarden
+      v-on:edit:garden="submitEdit"
+      :id="this.id"
+      :name="this.name"
+      :lat="this.lat"
+      :lng="this.lng"
+      :width="this.width"
+      :height="this.height"
+    />
   </div>
 </template>
 
@@ -17,15 +25,28 @@ export default {
     id: {
       type: Number,
     },
+    name: {
+      type: String,
+    },
+    lat: {
+      type: Number,
+    },
+    lng: {
+      type: Number,
+    },
+    width: {
+      type: Number,
+    },
+    height: {
+      type: Number,
+    },
   },
   methods: {
     submitEdit(info) {
-      axios
-        .put("/garden/gardenupdate", {
-          id: this.id,
-          info: info,
-        })
-        .then((garden) => console.log(garden));
+      axios.put("/garden/gardenupdate", {
+        id: this.id,
+        info: info,
+      });
     },
   },
 };
