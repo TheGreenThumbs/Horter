b82750c4dba28ec199a8fa08a88459ae836149d3
Lorie Zepeda <lzepeda.it@gmail.com>
2020-09-05 11:47:49 -0500

    (feat) Search queries Trefle API

--- client/src/components/WishList.vue
+++ client/src/components/WishList.vue
@@ -1,32 +1,78 @@
+<!-- Renders search field and search results -->
+
 <template>
   <div class="card">
     <div class="card-header">
       <p class="card-header-title">WISH LIST</p>
     </div>
     <div class="card-content">
+      <!-- Search field begins -->
       <b-field>
         <b-input
-          placeholder="Search..."
+          v-model="keyword"
           type="search"
+          placeholder="Search..."
           icon="magnify"
           icon-clickable
           @icon-click="searchIconClick"
         >
         </b-input>
       </b-field>
+      <!-- Search field ends -->
+      <!-- Search results begins -->
+      <div v-if="loaded"></div>
+      <div v-else>
+        <wishListSkeleton></wishListSkeleton>
+      </div>
+      <!-- Search results ends -->
+
+      <!-- <suspense>
+        <template #default> -->
+      <!-- </template>
+        <template #fallback>
+          <wishListSkeleton></wishListSkeleton>
+        </template>
+      </suspense> -->
     </div>
   </div>
 </template>
+
 <script>
+import WishListSkeleton from "./WishListSkeleton.vue";
+import axios from "axios";
+
 export default {
   name: "Wish",
+  components: {
+    wishListSkeleton: WishListSkeleton,
+  },
   methods: {
     searchIconClick() {
-      alert("You wanna make a search?");
+      axios
+        .get("/api/search", {
+          params: {
+            q: this.keyword,
+          },
+        })
+        .then((res) => {
+          results = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      this.keyword = "";
     },
     clearIconClick() {
       this.search = "";
     },
+  },
+  data() {
+    return {
+      loaded: false,
+      keyword: "",
+      results: [],
+      rounded: true,
+    };
   },
 };
 </script>
