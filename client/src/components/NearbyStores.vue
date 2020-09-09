4c223cf6fb6b2b8a80ffc9cc39f620cadd98b781
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-09-08 19:33:15 -0500

    (feat) nearby stores component renders a google map with nearby stores, will display indepth data on one store

--- client/src/components/NearbyStores.vue
+++ client/src/components/NearbyStores.vue
@@ -1,5 +1,83 @@
-<template></template>
+<template>
+  <div>
+    <GmapMap
+      :center="center"
+      :zoom="12"
+      map-type-id="terrain"
+      style="width: 800px; height: 500px"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="findStore(index)"
+      />
+    </GmapMap>
+    <div v-if="selected">
+      <div>
+        {{ selectedStore.name }}
+      </div>
+      <div>
+        {{ selectedStore.address }}
+      </div>
+      <div>
+        {{ selectedStore.phone }}
+      </div>
+      <div>
+        {{ selectedStore.schedule }}
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  name: "NearbyStores",
+  data() {
+    return {
+      center: {
+        lat: 29.9855645,
+        lng: -90,
+      },
+      markers: [
+        {
+          position: {
+            lat: 29.9855645,
+            lng: -90.1027271,
+          },
+        },
+      ],
+      selectedStore: {},
+      selected: false,
+    };
+  },
+  mounted() {
+    axios
+      .get("/stores", {
+        lat: 30,
+        lng: -90,
+      })
+      .then((stores) => {
+        this.markers = stores.data;
+      })
+      .catch((err) => console.log(err));
+  },
+  methods: {
+    findStore(index) {
+      this.selected = true;
+      axios({
+        method: "GET",
+        url: "/stores/one",
+        params: {
+          placeId: this.markers[index].placeId,
+        },
+      }).then((store) => {
+        this.selectedStore = store.data;
+      });
+    },
+  },
+};
 </script>
