d5a997b469fc5dfb951164ae8a2cd1b6c9865940
jazsmith <jazminetsmith@gmail.com>
2020-09-15 14:39:53 -0500

    (add) friends list component updated

--- client/src/components/Profile/FriendSearch.vue
+++ client/src/components/Profile/FriendSearch.vue
@@ -1,15 +1,61 @@
 <template>
   <div>
-    <div class="ui stackable grid container" :user0="user0">
+    <div class="pad-15-hor pad-15-ver header">
+      <div>
+        <img
+          src="https://listimg.pinclipart.com/picdir/s/15-153148_search-magnifying-glass-icon-black-search-icon-png.png"
+          width="25px"
+        />Search Friends
+      </div>
+    </div>
+    <div class="ui stackable grid container">
       <div class="four wide column">
-        <input name="search" type="text" placeholder="Search Friend" />
+        <div class="pad-15-hor pad-15-ver search-parent">
+          <div class="search-bar">
+            <input
+              name="friend"
+              v-model="friend"
+              v-on:change="searchFriend(friend)"
+              placeholder="Search by username"
+              lazy
+            />
+            <div
+              class="friendimg"
+              v-if="searched.username"
+              @click="goToUser(searched.id)"
+            >
+              <b-image
+                :src="searched.s3_id"
+                padding="15%"
+                ratio="4x4"
+                rounded
+              />
+              <p>{{ searched.username }}</p>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="eight wide column">
+        <div>
+          <img
+            src="https://www.clipartmax.com/png/middle/427-4273239_friends-icon-friends-icon-transparent-background.png"
+            width="25px"
+          />Friends List
+        </div>
         <div class="ui segment">
-          <img class="userimage" src="user0.photoUrl" />
-          <p type="text" @click="$router.push('FriendProfile')">
-            @{{ user0.username }}
-          </p>
+          <!-- <button @click="seeFriends()">Show Friends</button> -->
+          <div v-if="friends" class="friend-list">
+            <div
+              class="friends"
+              @click="goToUser(friend.id)"
+              :index="i"
+              v-for="(friend, i) in friends"
+              :key="i"
+            >
+              <b-image :src="friend.s3_id" padding="15%" ratio="2x2" rounded />
+              <p>{{ friend.username }}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -17,15 +63,66 @@
 </template>
 
 <script>
-import user0 from "../fake-data/fake-data.js";
 import FriendProfile from "./friend-profile.vue";
+import axios from "axios";
 
 export default {
   name: "FriendSearch",
+
+  props: ["user"],
+
   data() {
     return {
-      user0: user0,
+      searched: {},
+      friend: "",
+      friends: [],
     };
+  },
+
+  mounted() {
+    axios({
+      method: "GET",
+      url: "/friend/",
+      params: {
+        userId: this.user.id,
+      },
+    })
+      .then((friends) => {
+        const { data } = friends;
+        console.log("data", data);
+        this.friends = data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+
+  methods: {
+    searchFriend(username) {
+      axios({
+        method: "GET",
+        url: "/user/getuser",
+        params: { username },
+      })
+        .then((user) => {
+          const { data } = user;
+          this.searched = data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    goToUser(user) {
+      console.log("USER CLICKED");
+      this.$router
+        .push({
+          path: "/UserProfile",
+          query: { id: user },
+        })
+        .catch(() => {
+          this.$log.info("same route");
+        });
+    },
   },
 };
 </script>
@@ -36,5 +133,12 @@ input[type="text"] {
   background-position: 10px 10px;
   background-repeat: no-repeat;
   padding-left: 80px;
+}
+
+.header {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  padding-top: 10%;
 }
 </style>
