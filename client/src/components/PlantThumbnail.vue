4a5ee8f9509f0c6f36a5b68b0e07bd17e4374da0
Lorie Zepeda <lzepeda.it@gmail.com>
2020-09-15 18:51:16 -0500

    (refactor) placed plant client/src/components/PlantThumbnail.vuethumbnail into its own component

---
+++ client/src/components/PlantThumbnail.vue
@@ -0,0 +1,52 @@
+<!-- Renders plant thumbnail and plant modal -->
+
+<template>
+  <div class="thumbnail">
+    <div v-if="plant.image_url">
+      <a class="image is-64x64" @click="imageClick(plant.image_url)">
+        <b-image
+          :src="plant.image_url"
+          ratio="4by4"
+          :rounded="rounded"
+          :value="plant.image_url"
+          v-model="modalImageUrl"
+        ></b-image>
+      </a>
+    </div>
+    <div v-else>
+      <p class="image is-64x64">
+        <b-skeleton
+          circle
+          width="64px"
+          height="64px"
+          :animated="false"
+        ></b-skeleton>
+      </p>
+    </div>
+    <b-modal v-model="isImageModalActive" @close="isImageModalActive = false">
+      <p>
+        <img :src="modalImageUrl" />
+      </p>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PlantThumbnail",
+  data() {
+    return {
+      rounded: true,
+      isImageModalActive: false,
+      modalImageUrl: "",
+    };
+  },
+  props: ["plant"],
+  methods: {
+    imageClick(imageUrl) {
+      this.isImageModalActive = true;
+      this.modalImageUrl = imageUrl;
+    },
+  },
+};
+</script>
