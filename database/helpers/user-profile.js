40d7266955ddd2100fa99a3d652e855b9750876d
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-09-15 17:02:44 -0500

    (feat)Fuzzy Search User Names

--- database/helpers/user-profile.js
+++ database/helpers/user-profile.js
@@ -1,5 +1,5 @@
 const { models } = require("../index");
-
+const { Op } = require("sequelize");
 const { User } = models;
 
 /**
@@ -48,7 +48,11 @@ const findUserByGoogleId = (idGoogle) =>
 const findUserByUsername = (username) =>
   new Promise((resolve, reject) => {
     User.findOne({
-      where: { username },
+      where: {
+        username: {
+          [Op.iLike]: `%${username}%`,
+        },
+      },
       rejectOnEmpty: true,
     })
       .then((user) => {
