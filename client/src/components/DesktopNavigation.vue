0c06543114f6c2b649f68231573fc1edff737b7b
dtroyano86@gmail.com <dtroyano86@gmail.com>
2020-09-16 14:57:07 -0500

    (feat)Style Desktop Navigation

---
+++ client/src/components/DesktopNavigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        Horter
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item tag="router-link" :to="{ path: 'UserProfile' }">
+        Profile
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: 'plant' }">
+        Plantlist
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: 'wish' }">
+        Wishlist
+      </b-navbar-item>
+      <b-navbar-item tag="router-link" :to="{ path: 'FriendSearch' }">
+        Search Friends
+      </b-navbar-item>
+    </template>
+    <template slot="end">
+      <b-navbar-item v-if="!user.id" tag="a" href="/auth/google">
+        Login
+      </b-navbar-item>
+      <b-navbar-item v-else tag="a" href="/auth/logout"> Logout </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "DesktopNavigation",
+  props: ["user"],
+};
+</script>
+
+<style lang="sass"></style>
